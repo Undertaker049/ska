003c05d68511dfc088048fb6984f2a7547691a1a
alexandr <arumskiy@gmail.com>
2024-07-03 13:42:19 +0300

    wip

--- self_assessment/static/JS/self-assessment-commons.js
+++ self_assessment/static/JS/self-assessment-commons.js
@@ -24,25 +24,3 @@ function move_to_another_block(parent_id, shift, page_blocks) {
 function update_button_counter(button_id, page_object, name) {
     document.getElementById(button_id).textContent = `${name} (${page_object._count}/${page_object._total})`;
 }
-
-/**
- * Функция для отображения сообщений об ошибках
- * @param {String}text Текст сообщения
- * @returns {Promise<void>}
- */
-async function show_warning(text) {
-    const errorBox = document.getElementById("error-box");
-    errorBox.textContent = text;
-
-    errorBox.style.display = "block";
-    errorBox.style.transition = "opacity 0.5s";
-    errorBox.style.opacity = '1';
-
-    await new Promise(r => setTimeout(r, 5000));
-
-    errorBox.style.opacity = '0';
-
-    await new Promise(r => setTimeout(r, 500));
-
-    errorBox.style.display = "none";
-}
