167c0752eecffd311a603f4710099831abd432f9
Undertaker049 <kel.luc@yandex.ru>
2024-11-13 18:26:03 +0300

    Update(auth, employee_evaluation, admin, models)
    - Add department selection field to registration form and save this information into database
    
    - Fix password re-entry errors
    
    - Use fetch API to send form data
    
    - Improve error handling when submitting a form
    
    - Add verification of the minimum length of the username and password
    
    - Implement a visual indication of password mismatch by changing the color of the field borders
    
    - Ability to filter and search by department in the Django admin panel
    
    - Update the appearance of Employee_evaluation and About pages to display information about department the employee belongs to

--- employee_evaluation/templatetags/employee_evaluation_filters.py
+++ employee_evaluation/templatetags/employee_evaluation_filters.py
@@ -5,6 +5,20 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
+@register.filter
+def multiply(value, arg):
+    return float(value) * float(arg)
+
+
+@register.filter
+def divide(value, arg):
+
+    try:
+        return float(value) / float(arg)
+
+    except (ValueError, ZeroDivisionError):
+        return 0
+
 
 @register.filter
 @stringfilter
--- self_assessment/admin.py
+++ self_assessment/admin.py
@@ -1,15 +1,73 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import *
 
-admin.site.register(Employees)
+
+@admin.register(Employees)
+class EmployeesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department', 'is_supervisor', 'subordinate_of')
+    list_filter = ('department', 'is_supervisor')
+    search_fields = ('name', 'department__name')
+    list_per_page = 20
+    ordering = ('name',)
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(SkillsHW)
+class SkillsHWAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'get_department', 'product', 'get_score')
+    list_filter = ('employee__department', 'product')
+    search_fields = ('employee__name', 'product__name')
+
+    def get_department(self, obj):
+        return obj.employee.department
+
+    get_department.short_description = 'Отдел'
+    get_department.admin_order_field = 'employee__department'
+
+    def get_score(self, obj):
+        return obj.get_score()
+
+    get_score.short_description = 'Оценка'
+
+
+@admin.register(SkillsSW)
+class SkillsSWAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'get_department', 'product', 'get_score')
+    list_filter = ('employee__department', 'product')
+    search_fields = ('employee__name', 'product__name')
+
+    def get_department(self, obj):
+        return obj.employee.department
+
+    get_department.short_description = 'Отдел'
+    get_department.admin_order_field = 'employee__department'
+
+    def get_score(self, obj):
+        return obj.get_score()
+
+    get_score.short_description = 'Оценка'
+
+
+@admin.register(SkillsPR)
+class SkillsPRAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'get_department', 'process', 'level')
+    list_filter = ('employee__department', 'process')
+    search_fields = ('employee__name', 'process__name')
+
+    def get_department(self, obj):
+        return obj.employee.department
+
+    get_department.short_description = 'Отдел'
+    get_department.admin_order_field = 'employee__department'
+
 admin.site.register(Levels)
 admin.site.register(Hardware)
 admin.site.register(Software)
 admin.site.register(Processes)
 admin.site.register(TaskHW)
 admin.site.register(TaskSW)
-admin.site.register(SkillsHW)
-admin.site.register(SkillsSW)
-admin.site.register(SkillsPR)
