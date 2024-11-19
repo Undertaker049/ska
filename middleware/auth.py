379756868bca4a57783924a643bc7921eccc7f59
Undertaker049 <kel.luc@yandex.ru>
2024-11-20 01:53:26 +0300

    Fix (middleware\auth.py)
    Fix an error related to remembering public_url and further redirecting user to them after logging in

--- middleware/auth.py
+++ middleware/auth.py
@@ -1,18 +1,18 @@
 from django.shortcuts import redirect
 from django.conf import settings
-from django.urls import reverse
 
 
 class AuthenticationMiddleware:
 
+
     def __init__(self, get_response):
         self.get_response = get_response
+
 
     def __call__(self, request):
 
         if not request.user.is_authenticated:
             public_urls = [
-                '/auth/',
                 '/auth/registration',
                 '/static/',
                 '/media/',
@@ -21,10 +21,9 @@ class AuthenticationMiddleware:
 
             current_url = request.path_info
 
-            if not any(current_url.startswith(url) for url in public_urls):
+            if not any(current_url.startswith(url) for url in public_urls) and current_url != settings.LOGIN_URL:
                 next_url = request.get_full_path()
                 login_url = f"{settings.LOGIN_URL}?next={next_url}"
-
                 return redirect(login_url)
 
         response = self.get_response(request)
